c3c021d5d40d7ad5297859bea2331fbfb5e71b0d
Harish KM <harishkm@harishkm.in>
2021-07-07 11:21:13 +0530

    Define security group rules for Spring PetClinic server

---
+++ spring-petclinic-security-group.tf
@@ -0,0 +1,17 @@
+module "spring_petclinic_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.3.0"
+
+  name        = "spring_petclinic_security_group"
+  description = "Spring PetClinic security group."
+
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = ["${module.jenkins_server.private_ip[0]}/32"]
+  ingress_rules       = ["ssh-tcp"]
+  egress_rules        = ["all-all"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
