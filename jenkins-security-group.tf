df90917374f1bb775486482c408371b20f4d477d
Harish KM <harishkm@harishkm.in>
2021-07-07 20:19:57 +0530

    Allow ingress from everywhere to Jenkins

--- jenkins-security-group.tf
+++ jenkins-security-group.tf
@@ -6,7 +6,7 @@ module "jenkins_security_group" {
   description = "Jenkins security group."
 
   vpc_id              = module.vpc.vpc_id
-  ingress_cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["ssh-tcp", "http-8080-tcp"]
   egress_rules        = ["all-all"]
 
