0869341d3321a0cf534b747b4dee73d73a434e6c
Harish KM <harishkm@harishkm.in>
2021-07-07 11:20:42 +0530

    Define security group rules for Jenkins server

---
+++ jenkins-security-group.tf
@@ -0,0 +1,17 @@
+module "jenkins_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.3.0"
+
+  name        = "jenkins_security_group"
+  description = "Jenkins security group."
+
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+  ingress_rules       = ["ssh-tcp", "http-8080-tcp"]
+  egress_rules        = ["all-all"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
