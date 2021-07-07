2141e4d55248260adb34ffd9cfbbb8397894dd06
Harish KM <harishkm@harishkm.in>
2021-07-07 12:27:34 +0530

    Add Docker to Jenkins server

--- jenkins-server-init.sh
+++ jenkins-server-init.sh
@@ -14,6 +14,12 @@ apt install -y jenkins
 systemctl enable jenkins
 systemctl start jenkins
 
-# Ansible
+# Docker
 
-apt install -y ansible
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+apt update -y
+apt install -y docker-ce
+usermod -aG docker ubuntu
